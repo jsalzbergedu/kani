3dfe95050ca4c8b3a7573eb7a577e0c8ed3a33a4
Jacob Salzberg <salzber@amazon.com>
2024-06-07 14:53:24 -0400

    Make double_alias_fail.rs contract-based

--- tests/expected/aliasing_and_mutability/double_alias_fail.rs
+++ tests/expected/aliasing_and_mutability/double_alias_fail.rs
@@ -1,14 +1,26 @@
-// From the stacked borrows paper.
-fn example1(x: &mut i32, y: &mut i32) -> i32 {
-    *x = 42;
-    *y = 13;
-    return *x;
+// Created by Jacob Salzberg
+
+#[kani::modifies(x)]
+#[kani::modifies(y)]
+#[kani::requires(*x == x_old && *y == y_old)]
+#[kani::ensures((y_old < 100 && *y == 0) || (y_old >= 100 && *y == y_old))]
+fn reader_writer(x: &mut i32, y: &mut i32, x_old: i32, y_old: i32) {
+    assert_eq!(*x, x_old);
+    assert_eq!(*y, y_old);
+    if *y < 100 {
+        *x = 0;
+    } else {
+        *x = 1;
+    }
+    *y = *y * *x;
+    return;
 }
 
-#[kani::proof]
+// The above SHOULD optimize to if *y < 100 0 else *y, but an unsafe write will ruin this.
+#[kani::proof_for_contract(reader_writer)]
 fn main() {
-    let mut local = 5;
-    let raw_pointer = &mut local as *mut i32;
-    let result = unsafe { example1(&mut *raw_pointer, &mut *raw_pointer) };
-    assert!(local == 13);
+    let mut x = 1000;
+    let x_ptr = &mut x as *mut i32;
+    unsafe{reader_writer(&mut *x_ptr, &mut *x_ptr, *x_ptr, *x_ptr) }
+    assert!(x == 1);
 }
