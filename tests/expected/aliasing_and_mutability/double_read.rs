ed7d344504a55059980cf9a1aa7cff6b3917a167
Jacob Salzberg <salzber@amazon.com>
2024-06-07 15:03:37 -0400

    Change 2x read into contract based

--- tests/expected/aliasing_and_mutability/double_read.rs
+++ tests/expected/aliasing_and_mutability/double_read.rs
@@ -1,10 +1,12 @@
 // Created by Jacob Salzberg
 
+#[kani::requires(*y < i32::MAX - *x)]
+#[kani::ensures(result == *x + *y)]
 fn reader(x: &i32, y: &i32) -> i32 {
     return *x + *y;
 }
 
-#[kani::proof]
+#[kani::proof_for_contract(reader)]
 fn main() {
     let x = 10;
     let x_ptr = &x as *const i32;
