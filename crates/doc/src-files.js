a949b3e62e52be65d62b6d52233d9435c6585fa0
jsalzbergedu <jssalzbe@ncsu.edu>
2024-08-30 21:57:11 +0000

    Deploying to gh-pages from @ jsalzbergedu/kani@e33d6e933a0f9ef386f91cd68a37ecde0e383d9e 🚀

---
+++ crates/doc/src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["kani",["",[["models",[],["mod.rs"]]],["aliasing.rs","arbitrary.rs","contracts.rs","futures.rs","invariant.rs","lib.rs","mem_init.rs","shadow.rs","slice.rs","vec.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[169]}
